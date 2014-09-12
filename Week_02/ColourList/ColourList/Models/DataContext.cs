07fb705ff7cf6e5635244619d9654b13518f1f48
peteratseneca <peter.mcintyre@senecacollege.ca>
2014-09-12 07:29:11 -0400

    ColourList, the first code example

---
+++ Week_02/ColourList/ColourList/Models/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// more...
+using System.Data.Entity;
+
+namespace ColourList.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("name=DefaultConnection") { }
+
+        public DbSet<Colour> Colours { get; set; }
+    }
+
+    public class StoreInitializer : DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            Colour red = new Colour() { ColourName = "Red" };
+            context.Colours.Add(red);
+
+            Colour green = new Colour() { ColourName = "Green" };
+            context.Colours.Add(green);
+
+            context.SaveChanges();
+
+        }
+    }
+}
+
+
+
+
