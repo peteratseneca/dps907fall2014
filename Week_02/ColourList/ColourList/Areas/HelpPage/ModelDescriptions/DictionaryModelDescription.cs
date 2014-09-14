d4ea0f7c7483335030c4cd6075a914a33be3c872
peteratseneca <peter.mcintyre@senecacollege.ca>
2014-09-14 09:40:03 -0400

    ColourList, the first code example

--- Week_02/ColourList/ColourList/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ColourList.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Week_02/ColourList/ColourList/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ColourList.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
